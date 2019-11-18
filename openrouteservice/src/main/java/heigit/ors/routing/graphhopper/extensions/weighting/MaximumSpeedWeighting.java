29c7dddee579e03862e0c03bb76ced203a637fcd
Athanasios Kogios <51202721+star-pirate@users.noreply.github.com>
2019-11-18 15:11:29 +0100

    Fix speed conversion in CH edges.

--- openrouteservice/src/main/java/heigit/ors/routing/graphhopper/extensions/weighting/MaximumSpeedWeighting.java
+++ openrouteservice/src/main/java/heigit/ors/routing/graphhopper/extensions/weighting/MaximumSpeedWeighting.java
@@ -21,9 +21,21 @@ public class MaximumSpeedWeighting extends FastestWeighting {
         weighting = map.get("weighting","fastest");
     }
 
-    private double speedToTime(double speed, EdgeIteratorState edge){
+    private double speedToTime_km_h(double speed, EdgeIteratorState edge){
         //Conversion of the speeds taken from the edges into time adding the penalties
         double time = edge.getDistance() / speed * SPEED_CONV;
+
+        // add direction penalties at start/stop/via points
+        boolean unfavoredEdge = edge.getBool(EdgeIteratorState.K_UNFAVORED_EDGE, false);
+        if (unfavoredEdge)
+            time += headingPenalty;
+
+        return time;
+    }
+
+    private double speedToTime_m_s(double speed, EdgeIteratorState edge){
+        //Conversion of the speeds taken from the edges into time adding the penalties
+        double time = edge.getDistance() / speed;
 
         // add direction penalties at start/stop/via points
         boolean unfavoredEdge = edge.getBool(EdgeIteratorState.K_UNFAVORED_EDGE, false);
@@ -38,7 +50,7 @@ public class MaximumSpeedWeighting extends FastestWeighting {
         CHEdgeIteratorState tmp = (CHEdgeIteratorState) edge;
         if (tmp.isShortcut()) {
             //If a shortcut is in both directions the weight is identical => no need for 'reverse'
-            //tmp.getWeight() is in time so we first change it to speed
+            //tmp.getWeight() is in time (seconds) so we first change it to speed
             double time = tmp.getWeight();
             double distance = edge.getDistance();
             double speed = edge.getDistance()/ tmp.getWeight();
@@ -49,7 +61,7 @@ public class MaximumSpeedWeighting extends FastestWeighting {
             //Find the minimum of the two values. if (speed > userMaxSpeed) -> userMaxSpeed, else -> speed
             speed =  java.lang.Math.min(userMaxSpeed, speed);
             //Convert speed to time
-            return speedToTime(speed, edge);
+            return speedToTime_m_s(speed, edge);
         }
         else{
             //If it is not a shortcut we need to test both directions
@@ -59,7 +71,7 @@ public class MaximumSpeedWeighting extends FastestWeighting {
             }
             if(speed > userMaxSpeed) {
                 speed = userMaxSpeed;
-                return speedToTime(speed, edge);
+                return speedToTime_km_h(speed, edge);
             }else{
                 return super.calcWeight(edge, reverse, prevOrNextEdgeId);
             }
